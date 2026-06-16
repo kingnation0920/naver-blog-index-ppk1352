7ca3641bea619e720b45bfe3f81aa39465f047c7
kingnation0920 <57610017+kingnation0920@users.noreply.github.com>
2026-06-17 08:34:50 +0900

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://naver-blog-index-ppk1352.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 1,
+    },
+  ];
+}
